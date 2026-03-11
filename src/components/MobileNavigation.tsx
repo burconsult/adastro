68dfa3cd343fc32139fad8b58e8642b691d5b707
burconsult <admin@burconsult.com>
2026-03-11 10:31:01 +0200

    fix: localize public theme toggle label

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -231,7 +231,7 @@ export default function MobileNavigation({
             ))}
           </select>
         )}
-        <ModeToggle />
+        <ModeToggle label={toggleThemeLabel} />
         {isAdmin && adminLink && (
           <a
             href={adminLink.href}
